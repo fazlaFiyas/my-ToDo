444c89b294356fe18f338cdf196197d784a6bc0d
fazlaFiyas <fazlafiyas20011220@gmail.com>
2025-02-12 06:03:27 +0530

    adding a floating action button and a text field to add new todos

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -9,15 +9,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final _controller = TextEditingController();
+
   final List toDoList = [
     ['Learn Flutter', false],
     ['Return library book', false],
     ['Complete notes', false],
   ];
 
-  void checkBoxChanged(int index){
+  void checkBoxChanged(int index) {
     setState(() {
       toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void saveNewTask(){
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
     });
   }
 
@@ -32,14 +42,50 @@ class _HomePageState extends State<HomePage> {
         foregroundColor: Colors.white,
       ),
       body: ListView.builder(
-        itemCount:toDoList.length,
-        itemBuilder: (BuildContext context, index){
-        return TodoList(
-          taskName: toDoList[index][0],
-          taskCompleted: toDoList[index][1],
-          onChanged: (value)=> checkBoxChanged(index),
-        );
-      }),
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return TodoList(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(index),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'Add a new ToDo',
+                  filled: true,
+                  fillColor: Colors.white54,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: saveNewTask,
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
     );
   }
 }
